bb2637fcb63bc21d8edc983ab9b29b4c9b7597c0
justindavisa <justindavisa@gmail.com>
2019-03-01 14:30:10 -0500

    Refactor for multiple methods started

--- village/src/App.js
+++ village/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { NavLink, Route } from 'react-router-dom';
+import { NavLink, Route, withRouter } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -15,7 +15,7 @@ class App extends Component {
       error: ''
     };
   }
-  
+
   componentDidMount() {
     console.log('CDM now running');
     axios
@@ -30,44 +30,61 @@ class App extends Component {
       });
   }
 
-  // addSmurf = (e, smurf) => {
-  //   e.peventDefault();
-  //   axios
-  //     .post('http://localhost:3333/smurfs', smurf)
-  //     .then(res => {
-  //       this.setState({
-  //         smurfs: res.data
-  //       });
-  //       this.props.history.push('/smurf-list')
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // }
+  addSmurf = (e, smurf) => {
+    e.peventDefault();
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(res => {
+        this.setState({
+          smurfs: res.data
+        });
+        this.props.history.push('/smurfs')
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  deleteSmurf = (e, id) => {
+    e.preventDefault();
+    console.log('now in deleteSmurf');
+    axios
+      .delete(`http://localhost:3333/smurfs/${id}`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          items: res.data
+        });
+        this.props.history.push('/smurfs');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   render() {
     return (
       <div className="App">
 
         <div className='nav'>
-          <NavLink to = '/smurf-form'>Add Smurf</NavLink>
-          <NavLink to = '/'>Home</NavLink>
+          <NavLink to='/smurf-form'>Add Smurf</NavLink>
+          <NavLink to='/'>Home</NavLink>
         </div>
 
-        <Route 
-          path = '/smurf-form'
-          render = {props => (
+        <Route
+          path='/smurf-form'
+          render={props => (
             <SmurfForm
               {...props}
             />
           )}
         />
-        <Route 
-          exact path = '/'
-          render = {props => (
+        <Route
+          exact path='/'
+          render={props => (
             <Smurfs
               {...props}
-              smurfs = {this.state.smurfs}
+              smurfs={this.state.smurfs}
             />
           )}
         />
